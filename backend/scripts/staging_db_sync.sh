61a80cb01e263f40f85bcfa95daef37e5399fe1c
Brent Janski <31578457+talktobrent@users.noreply.github.com>
2021-03-31 21:09:29 -0500

    Delete backend/scripts directory

--- backend/scripts/staging_db_sync.sh
+++
@@ -1,3 +0,0 @@
-echo "============ Syncing stage DB to local ============"
-PGPASSWORD=$(cat .staging_db_password) pg_dump -c -h [DB_LOCATION] -U [username] [db] | PGPASSWORD=[password] psql -h db -U postgres postgres
-echo "============ Done! ============"
