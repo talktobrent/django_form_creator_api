7907955ba65daf441bd2324cc466f849ad25dbd8
Brent Janski <31578457+talktobrent@users.noreply.github.com>
2021-03-31 21:09:59 -0500

    Delete backend/apps/users directory

--- backend/apps/users/tests.py
+++
@@ -1,6 +0,0 @@
-from django.test import TestCase
-
-
-class SampleTestCase(TestCase):
-    def test_one_equals_one(self):
-        self.assertEqual(1, 1)
--- backend/apps/users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from rest_framework import routers
-from .views import UserViewSet
-
-# Users API
-router = routers.DefaultRouter()
-router.trailing_slash = '/?'
-
-router.register(r'users', UserViewSet)
