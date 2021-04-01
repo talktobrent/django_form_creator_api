04ff3df38d8650dd9d7c808a3f6c951a35dadbb2
Brent Janski <31578457+talktobrent@users.noreply.github.com>
2021-03-31 21:13:00 -0500

    Delete tests.py

--- backend/apps/form_creator/tests.py
+++
@@ -1,7 +0,0 @@
-# from django.test import TestCase
-# from rest_framework.test import APIRequestFactory
-#
-# # Create your tests here.
-#
-# factory = APIRequestFactory()
-# request = factory.post('/api/form', '{"test1":[{"name":"gggg","required":false,"order":0,"value":0,"type":"integer"},{"option":[{"value":"6","selected":false,"text":"tttt"},{"value":4, "text":"ggggyyy"}],"name":"dddd","required":false,"type":"select"}]}', format='json')
